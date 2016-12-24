5a8b543617d1dc79d1d68da4d2283f9c30044ef9
Kailang Fu <kailangfu@gmail.com>
2016-12-24 11:21:17 -0600

    Comply with GCS

--- lab_intro/rgbapixel.cpp
+++ lab_intro/rgbapixel.cpp
@@ -1,7 +1,7 @@
+// Copyright 2016 Kailang Fu
+
 #include "rgbapixel.h"
 
-RGBAPixel::RGBAPixel() : red(0xff), green(0xff), blue(0xff), alpha(0xff) {
-}
+RGBAPixel::RGBAPixel() : red(0xff), green(0xff), blue(0xff), alpha(0xff) { }
 
-RGBAPixel::RGBAPixel(uint8_t r, uint8_t g, uint8_t b) : red(r), green(g), blue(b), alpha(0xff) {
-}
+RGBAPixel::RGBAPixel(uint8_t r, uint8_t g, uint8_t b) : red(r), green(g), blue(b), alpha(0xff) { }
--- lab_intro/rgbapixel.h
+++ lab_intro/rgbapixel.h
@@ -1,3 +1,5 @@
+// Copyright 2016 Kailang Fu
+
 #ifndef RGBAPIXEL_H_
 #define RGBAPIXEL_H_
 
@@ -5,22 +7,22 @@ typedef unsigned char uint8_t;
 
 class RGBAPixel {
  public:
-	// Byte for the red component of the pixel.
-	uint8_t red;
-	// Byte for the green component of the pixel.
-	uint8_t green;
-	// Byte for the blue component of the pixel.
-	uint8_t blue;
-	// Byte for the alpha component of the pixel.
-	uint8_t alpha;
+    // Byte for the red component of the pixel.
+    uint8_t red;
+    // Byte for the green component of the pixel.
+    uint8_t green;
+    // Byte for the blue component of the pixel.
+    uint8_t blue;
+    // Byte for the alpha component of the pixel.
+    uint8_t alpha;
 
-	// Constructs a default RGBAPixel.
-	// A default pixel is completely opaque (non-transparent) and white.
-	RGBAPixel();
+    // Constructs a default RGBAPixel.
+    // A default pixel is completely opaque (non-transparent) and white.
+    RGBAPixel();
 
-	// Constructs an opaque RGBAPixel with the given red, green, blue color values.
-	// The alpha component of the pixel constructed should be 255.
-	RGBAPixel(uint8_t red, uint8_t green, uint8_t blue);
+    // Constructs an opaque RGBAPixel with the given red, green, blue color values.
+    // The alpha component of the pixel constructed should be 255.
+    RGBAPixel(uint8_t red, uint8_t green, uint8_t blue);
 };
 
-#endif
+#endif  // RGBAPIXEL_H_
